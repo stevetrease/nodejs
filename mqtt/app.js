5ff675ce77857f5570ea74f4857d5159cf40e47b
Steve Trease <stevetrease@gmail.com>
2013-05-03 10:42:55 +0100

    MQTT and Sockets.io example, pushing CurrentCost consumption via Mosquitto

--- mqtt/app.js
+++ mqtt/app.js
@@ -1,9 +1,13 @@
-var app = require('http').createServer(handler),
-   io = require('socket.io').listen(app),
-   fs = require('fs')
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app, {
+	'log level': 1
+});
+var fs = require('fs');
 
 app.listen(8500);
 
+
+// server web pages
 function handler (req, res) {
 	console.log('Connection from %j', req.connection.remoteAddress);
 	fs.readFile(__dirname + '/index.html',
@@ -18,19 +22,22 @@ function handler (req, res) {
 }
 
 
+// for each client connection, subscribe to mqtt and push updates
 io.sockets.on('connection', function (socket) {
+
 	var mqtt = require('mqtt');
 	var client = mqtt.createClient(1883, 'localhost', function(err, client) {
   		keepalive: 10000
 	});
 
 	client.on('connect', function() {
-  		client.subscribe('sensors/power/0');
+  		client.subscribe('sensors/power/+');
 
 	  	client.on('message', function(topic, message) {
     			console.log('topic: ' + topic + ' payload: ' + message);
-  			socket.emit('data', { channel: 0, value: message });
+  			socket.emit('data', { topic: topic, value: message });
   		});
 	});
+
 });
 
